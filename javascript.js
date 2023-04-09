8219fab29f7d612ec7d23bb24b817fcf3938b503
Matt Kindig <mkindig97@gmail.com>
2023-04-09 01:15:38 -0400

    This is my functional pop-up script for my home page.

---
+++ javascript.js
@@ -0,0 +1,7 @@
+let str1 = 'Hello!';
+let str2 = 'Welcome';
+let str3 = 'to';
+let str4 = "Matt's";
+let str5 = 'new';
+let str6 = 'webpage';
+alert(str1 + " " + str2 + " " + str3 + " " + str4 + " " + str5 + " " + str6);
